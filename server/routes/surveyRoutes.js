7f52a2ebf426f2d7fc42934c03296162bec25281
Apostolos Gouvalas <apo.gouv@gmail.com>
2017-10-21 21:21:13 +0300

    Update records and keep track of survey answers

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -4,6 +4,9 @@
  * Author  : Apostolos Gouvalas
  * Date    : 20/10/2017
  */
+const _ = require('lodash');
+const Path = require('path-parser');
+const{ URL } = require('url');
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
@@ -15,8 +18,38 @@ const Survey = mongoose.model('surveys');
 
 module.exports = app => {
 
-  app.get('/api/surveys/thanks', (req, res) => {
+  app.get('/api/surveys/:surveyId/:choice', (req, res) => {
     res.send('Thanks for voting!');
+  });
+
+  app.post('/api/surveys/webhooks', (req, res) => {
+    const p = new Path('/api/surveys/:surveyId/:choice');
+
+    _.chain(req.body)
+      .map(({ email, url}) => {
+        // get the path name only and test it to the above
+        const match = p.test(new URL(url).pathname);
+        if (match){
+            return { email, surveyId: match.surveyId, choice: match.choice };
+        }
+      })
+      .compact()
+      .uniqBy( 'email', 'surveyId')
+      .each( ({ surveyId, email, choice }) => {
+        Survey.updateOne({
+          _id: surveyId,
+          recipients: {
+            $elemMatch: {email: email, responded: false}
+          }
+        },{
+          $inc: { [choice]: 1 },
+          $set: { 'recipients.$.responded': true },
+          lastResponded: new Date()
+        }).exec();
+      })
+      .value();
+
+    res.send({});
   });
 
   // create a new survey and send a big email
---
+++ server/sendgrid_webhook.js
@@ -0,0 +1,10 @@
+/**
+ * File    : sendgrid_webhook.js
+ * Project : fullstack-node-with-react
+ * Author  : Apostolos Gouvalas
+ * Date    : 21/10/2017
+ */
+const localtunnel = require('localtunnel');
+localtunnel(5000, { subdomain: 'feeeeedcollllla' }, function(err, tunnel) {
+  console.log('LT running')
+});
